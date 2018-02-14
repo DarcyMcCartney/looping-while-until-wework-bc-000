d61c6451c79e8e20fd94f08b6244ebc67445daf0
Darcy McCartney <darcy.mccartney@wework.com>
2018-02-14 01:03:23 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,16 @@
 def using_while
 	levitation_force = 6
-  while levitation_force += 1 
-  if counter = 10 
-    break
+  while levitation_force += 1
   puts "Wingardium Leviosa"
+  if counter >= 10 
+    break
 end
+end
+
+counter = 0
+while counter < 20
+  puts "The current number is less than 20."
+  counter += 1
 end
 
 
